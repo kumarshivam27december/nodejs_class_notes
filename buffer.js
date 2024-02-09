2febac20857f52cb9730aaeaffc0c706106219c3
Shivam Kumar <kumarshivam16062004@gmail.com>
2024-02-09 13:42:04 +0530

    buffer write in js

---
+++ buffer.js
@@ -0,0 +1,14 @@
+var buf = new Buffer(10);
+var buf = new Buffer([10,20,30,40]);
+var buf = new Buffer("simply easy learning","utf-8");
+
+
+/*
+//write buffer - concept
+
+buf = new Buffer.alloc(256);
+len = buf.write("simply easy learning");
+console.log("octet written: "+len);
+
+
+*/
