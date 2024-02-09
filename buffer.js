8540bb59d146cb61b9ddf0909587704b5fa87362
Shivam Kumar <kumarshivam16062004@gmail.com>
2024-02-09 14:11:42 +0530

    buffer concatinate

--- buffer.js
+++ buffer.js
@@ -1,7 +1,10 @@
+/*
+//create buffer
 var buf = new Buffer(10);
 var buf = new Buffer([10,20,30,40]);
 var buf = new Buffer("simply easy learning","utf-8");
 
+*/
 
 /*
 //write buffer - concept
@@ -38,10 +41,27 @@ console.log(json);
 */
 
 /*
+
 //concat buffer
-*/
 var buffer1 = new Buffer.from('TutorialsPoint');
 var buffer2 = new Buffer.from('simply easy learning');
 var buffer3 = Buffer.concat([buffer1, buffer2]);
 console.log("buffer 3 content: "+buffer3.toString()); //TutorialsPointsimply easy learning
 
+
+*/
+
+/*
+//compare  two buffers
+var buffer1 = new Buffer.from('ABC');
+var buffer2 = new Buffer.from('ABCD');
+var result  = buffer1.compare(buffer2);
+if(result<0){
+    console.log(buffer1 + "comes before " + buffer2);
+}else if(result === 0){
+    console.log(buffer1 + "is equal to" + buffer2)
+} else {
+    console.log(buffer1 + " comes after " + buffer2);
+}
+
+*/
