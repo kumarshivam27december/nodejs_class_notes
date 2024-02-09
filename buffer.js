70ce00f772c628e8e1b4e2e5d053f4ce856d705a
Shivam Kumar <kumarshivam16062004@gmail.com>
2024-02-09 14:29:46 +0530

    buffer slice

--- buffer.js
+++ buffer.js
@@ -75,3 +75,12 @@ if(result<0){
     buffer1.copy(buffer2);
     console.log("buffer2 content : "+buffer2.toString()) ;   
 */ 
+
+
+/*
+//buffer slicing
+var buffer1 = new Buffer('Tutorialspoint');
+//slicing a buffer
+var buffer2 = buffer1.slice(0,9);
+console.log("buffer2 content: "+buffer2.toString());
+*/
