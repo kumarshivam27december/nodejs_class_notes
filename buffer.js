0cfde3ac1dea180f7eff3533b91591494197f90a
Shivam Kumar <kumarshivam16062004@gmail.com>
2024-02-09 13:51:13 +0530

    read buffer

--- buffer.js
+++ buffer.js
@@ -12,3 +12,27 @@ console.log("octet written: "+len);
 
 
 */
+
+
+/*
+// read buffer
+
+buf = new Buffer.alloc(26);
+for(var i = 0;i<26;i++){
+    buf[i] = i+97;
+}
+console.log(buf.toString('ascii')); //prints simply easy learning in ascii
+console.log(buf.toString('ascii',0,5))//output abcde
+console.log(buf.toString('utf-8',0,5)); //prints the same result as above because utf-8
+console.log(buf.toString('undefined',0,5)); //prints the same result as above because utf-8
+
+
+
+//alter
+
+
+var buf = new Buffer('simply easy learning');
+var json = buf.toJSON(buf);
+console.log(json);
+
+*/
